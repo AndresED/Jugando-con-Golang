6b5661702087b6d4b405ae4f2d38557a98ba18cc
AndresED <andres30xed@gmail.com>
2017-12-07 08:53:42 -0500

    Jugando con operadores matematicos

---
+++ operadores-matematicos.go
@@ -0,0 +1,20 @@
+package main
+import (
+	"fmt"
+)
+
+func main(){
+	var num1 int =10;
+	var num2 int =2;
+	//SUMA
+	fmt.Println(num1+num2)  //DEVUELVE LA SUMA DE NUM1 Y NUM2
+	//RESTA
+	fmt.Println(num1-num2) //DEVUELVE LA RESTA DE NUM1 Y NUM2
+	//MULTIPLICACION
+	fmt.Println(num1*num2) // DEVUELVE LA MULTIPLICACION DE NUM1 YNUM2
+	//DIVICION
+	fmt.Println(num1/num2)  //DEVUELVE LA DIVISION DE NUM1 Y NUM2
+	// %
+	fmt.Println(num1%num2) //DEVUELVE EL RESIDUO DE LA DIVISION DE NUM1 Y NUM2
+	
+}
